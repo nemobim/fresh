82aa7e45f5697e5ed16c40cd9933cf51f202d9ad
nemo-nemo-bim <drakequation@naver.com>
2024-03-05 00:57:17 +0900

    [feat] 몽고디비 모듈화

--- src/app/board/[boardId]/page.tsx
+++ src/app/board/[boardId]/page.tsx
@@ -1,8 +1,19 @@
-const page = () => {
+import { getBoard } from "@/server/collection";
+import dayjs from "dayjs";
+
+const page = async ({ params }: any) => {
+  const { boardId } = params;
+  const boardData = await getBoard(boardId);
+
+  if (!boardData) return <div>게시판이 없습니다.</div>;
+
+  const date = dayjs(boardData.createdAt).format("YYYY-MM-DD HH:mm");
+
   return (
     <div>
-      <h1 className="text-4xl">board</h1>
-      <div className="p-4 bg-slate-200 rounded-md my-4">dslksdlkdslksdlkdskl</div>
+      <h1 className="text-4xl text-center">{boardData.title}</h1>
+      <p className="text-slate-700 mt-3 text-end">{date}</p>
+      <div className="p-4 bg-slate-200 rounded-md my-4 min-h-40">{boardData.content}</div>
     </div>
   );
 };
--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,17 +1,15 @@
 import Title from "@/elements/Title";
-import { connectDB } from "@/server/database";
+import { getBoardList } from "@/server/collection";
 import Link from "next/link";
 
 const page = async () => {
-  let client = await connectDB;
-  const db = client.db("freshDB");
-  let result = await db.collection("board").find().toArray();
+  const boardList = await getBoardList();
 
   return (
     <div>
       <Title title="게시판" color="text-orange-500" />
       <div>
-        {result.map((item) => (
+        {boardList.map((item) => (
           <div key={item.boardId} className="items-center p-3 shadow-md my-3 bg-gray-100 hover:bg-slate-100">
             <Link href={`/board/${item.boardId}`}>
               <div>
